f6eeeced8b5cba6d494ed9d006acdeceec241096
Fenhl <fenhl@fenhl.net>
2017-12-19 20:22:26 +0000

    Add support for continuing text via the indent parameter

--- text.lua
+++ text.lua
@@ -1,10 +1,10 @@
-function write_inner(font, simulate, text, size, min_x, max_x, min_y, max_y, halign, valign, r, g, b, a)
+function write_inner(font, simulate, text, size, min_x, max_x, min_y, max_y, indent, halign, valign, r, g, b, a)
     -- first, calculate y offset from number of lines
     local y
     if valign == "top" then
         y = min_y
     else
-        local text_height = write_inner(font, true, text, size, min_x, max_x, min_y, max_y, halign, "top", r, g, b, a).height
+        local text_height = write_inner(font, true, text, size, min_x, max_x, min_y, max_y, indent, halign, "top", r, g, b, a).height
         if valign == "middle" then
             y = min_y + ((max_y - min_y) - text_height) / 2
         else -- valign == "bottom"
@@ -19,7 +19,8 @@ function write_inner(font, simulate, text, size, min_x, max_x, min_y, max_y, hal
     local num_paragraphs = #text
     local num_words
     local test_line
-    local x
+    local x = indent
+    local final_indent = min_x
     for paragraph = 1, num_paragraphs do
         if paragraph > 1 then
             y = y + size * 1.5
@@ -37,7 +38,9 @@ function write_inner(font, simulate, text, size, min_x, max_x, min_y, max_y, hal
             end
             line_width = font:width(test_line, size)
             if line_width > (max_x - min_x) and line ~= "" then
-                if halign == "left" then
+                if x ~= nil then
+                    x = x
+                elseif halign == "left" then
                     x = min_x
                 elseif halign == "center" then
                     x = min_x + ((max_x - min_x) - font:width(line, size)) / 2
@@ -48,6 +51,7 @@ function write_inner(font, simulate, text, size, min_x, max_x, min_y, max_y, hal
                     font:write(x, y, line, size, r, g, b, a)
                 end
                 y = y + size
+                x = nil
                 height = height + size
                 line = text[paragraph][word]
                 if font:width(line, size) > width then
@@ -60,7 +64,9 @@ function write_inner(font, simulate, text, size, min_x, max_x, min_y, max_y, hal
                 end
             end
         end
-        if halign == "left" then
+        if x ~= nil then
+            x = x
+        elseif halign == "left" then
             x = min_x
         elseif halign == "center" then
             x = min_x + ((max_x - min_x) - font:width(line, size)) / 2
@@ -70,8 +76,13 @@ function write_inner(font, simulate, text, size, min_x, max_x, min_y, max_y, hal
         if not simulate then
             font:write(x, y, line, size, r, g, b, a)
         end
+        final_indent = x + font:width(line, size)
     end
-    return {width=width, height=height}
+    return {
+        width=width,
+        height=height,
+        final_indent=final_indent
+    }
 end
 
 return function(factory_args)
@@ -97,6 +108,7 @@ return function(factory_args)
             args.max_x or factory_args.width - (args.size or 100) / 2,
             args.min_y or (args.size or 100) / 2,
             args.max_y or factory_args.height - (args.size or 100) / 2,
+            args.indent,
             args.halign or "center",
             args.valign or "middle",
             red,
